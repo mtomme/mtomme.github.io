93d5a803354bfa8dbe458c0c82b25b91c976160b
ESghub2024 <159388740+ESghub2024@users.noreply.github.com>
2024-12-03 19:24:58 -0600

    Update chatbotAI.py

--- chatbotAI.py
+++ chatbotAI.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     query = "Under 30000"
     test_response = chatbot.get_chatgpt_response(csv_filepath="Organized_Car_Data/Chevrolet/Coupe.csv", user_query=query)
     print(test_response)
-else
+elif
     query = "Acura"
     df = pd.read_csv("Organized_Car_Data/Chevrolet/Coupe.csv")
     response = getchatgpt_response(df, query)
